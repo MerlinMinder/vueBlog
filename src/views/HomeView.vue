c714f2095e1b247acb6e4a1a09b6cb57b7862e3c
MerlinMinder <merlin.coding@gmail.com>
2022-03-24 18:42:09 +0100

    Implemented HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,39 @@
 <template>
-  <div id="page"></div>
+  <div id="page" v-if="blog1">
+    <div class="blogshow">
+      <div v-html="blog1['Blogwiz\nVue + Firebase'].img" class="blogimg"></div>
+      <div class="title">
+        {{ blog1["Blogwiz\nVue + Firebase"].title + "\n"
+        }}<RouterLink
+          class="link"
+          :to="`/posts/${blog1['Blogwiz\nVue + Firebase'].title}`"
+          >Visit Blog</RouterLink
+        >
+      </div>
+    </div>
+    <div class="blogshow">
+      <div class="title">
+        {{ blog1["Fitness App\n(My First App)"].title + "\n"
+        }}<RouterLink
+          class="link"
+          :to="`/posts/${blog1['Fitness App\n(My First App)'].title}`"
+          >Visit Blog</RouterLink
+        >
+      </div>
+      <div
+        v-html="blog1['Fitness App\n(My First App)'].img"
+        class="blogimg right"
+      ></div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-const sendData = async () => {
-  console.log("hi");
-};
+import { useBlogStore } from "../stores/stores";
+import { RouterLink } from "vue-router";
+const blogs = useBlogStore();
+
+const blog1 = blogs.blogs;
 </script>
 
 <style lang="scss" scoped>
@@ -13,5 +41,36 @@ const sendData = async () => {
 
 #page {
   background-color: $background;
+
+  .blogshow {
+    padding: 5vw;
+    height: 45vw;
+    display: flex;
+    flex-direction: row;
+
+    .blogimg {
+      width: 45vw;
+      height: 100%;
+      align-items: center;
+    }
+
+    .title {
+      width: 45vw;
+      font-size: 4vw;
+      font-weight: 800;
+      margin-top: 8vw;
+      white-space: pre-wrap;
+      text-align: center;
+
+      .link {
+        font-size: 2vw;
+        text-decoration: underline;
+      }
+    }
+    .right {
+      margin-right: 0px;
+      margin-left: auto;
+    }
+  }
 }
 </style>
